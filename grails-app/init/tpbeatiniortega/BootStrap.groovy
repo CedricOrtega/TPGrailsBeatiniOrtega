2f657ae484164389de694b89b29e70709ca66a01
CedricOrtega <cedric.ortega06210@gmail.com>
2019-10-01 09:11:13 +0200

    Version de connection avec jdbc et données de test ajouté dans bootstrap.groovy

--- grails-app/init/tpbeatiniortega/BootStrap.groovy
+++ grails-app/init/tpbeatiniortega/BootStrap.groovy
@@ -3,6 +3,23 @@ package tpbeatiniortega
 class BootStrap {
 
     def init = { servletContext ->
+        def userInstance = new User(username: "username",
+                password: "password",
+                thumbnail: new Illustration(filename: "thumb.png")).save(flush: true, failOnError: true)
+        1..5.each{
+            userInstance.addToAnnonces(
+                    new Annonce(
+                        title: "Title",
+                        description: "description",
+                        validTill: new Date(),
+                        state: Boolean.TRUE
+            )
+                            .addToIllustrations(new Illustration(filename: "filename"))
+                            .addToIllustrations(new Illustration(filename: "filename2"))
+                            .addToIllustrations(new Illustration(filename: "filename3"))
+            )
+        }
+        userInstance.save(flush:true, failOnError:true)
     }
     def destroy = {
     }
